63668f28b9c970fb95a426a43e01d0de65f0ac68
emmanouil <potetsie@imag.fr>
2016-01-06 16:55:34 +0100

    added logging to file function /w timestamps

--- app/src/main/java/com/tsarouchi/betaapp/UtilsClass.java
+++ app/src/main/java/com/tsarouchi/betaapp/UtilsClass.java
@@ -6,8 +6,11 @@ import android.os.Environment;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Calendar;
 
 /**
  * Created by Emmanouil on 17-Dec-15.
@@ -20,6 +23,7 @@ public class UtilsClass extends Application {
     private static File logFile;
     private static File sdCardDir;
     private static File mediaDir;
+    public enum LogLVL{ERROR, INFO, DEBUG}
 
     /*
      * Initialize
@@ -48,6 +52,58 @@ public class UtilsClass extends Application {
     }
 
 //Endof Getters
+
+//Startof Other Methods
+    public static void logERROR(String msg){
+        log(msg, LogLVL.ERROR);
+    }
+    public static void logDEBUG(String msg){
+        log(msg, LogLVL.DEBUG);
+    }
+    public static void logINFO(String msg){
+        log(msg, LogLVL.INFO);
+    }
+    private static void log(String msg, LogLVL lvl){
+        Calendar c = Calendar.getInstance();
+        String timestamp = c.get(Calendar.DATE)+"/"+(c.get(Calendar.MONTH)+1)+"  "+c.get(Calendar.HOUR_OF_DAY)+":"+c.get(Calendar.MINUTE)+":"+c.get(Calendar.MILLISECOND);
+        switch (lvl){
+            case ERROR:
+                Log.e(TAG, msg);
+                msg = "ERROR: "+msg;
+                break;
+            case DEBUG:
+                Log.d(TAG, msg);
+                msg = "DEBUG: "+msg;
+                break;
+            case INFO:
+                Log.i(TAG, msg);
+                break;
+            default:
+                Log.w(TAG, msg);
+                break;
+        }
+        if(getLogToFile()){
+            msg = "\n"+timestamp+" "+msg;
+            try
+            {
+                FileWriter fileWriter = new FileWriter(logFile,true);
+                BufferedWriter bufferWritter = new BufferedWriter(fileWriter);
+                bufferWritter.write(msg);
+                bufferWritter.close();
+            }
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                Log.e(TAG, "exception was thrown", e);
+            }
+
+
+        }
+
+
+    }
+//Endof Other Methods
+
 
     /*
      * Check if writing to ext is possible
@@ -88,7 +144,7 @@ public class UtilsClass extends Application {
             Log.e(TAG, "File Not Created - using Android Log for logging");
             return false;
         }else{
-            Log.i(TAG, "File "+fileList()[0]+" is used for logging");
+            logINFO("Starting new Log - File "+fileList()[0]+" is used for logging");
         }
         return true;
 
